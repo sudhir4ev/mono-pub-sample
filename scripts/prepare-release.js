383bd9f0dec9a7fa98998a39ecc0a36095008e08
sudhyr <>
2023-05-30 14:54:24 +0800

    chore(general): semantic release for selected package

---
+++ scripts/prepare-release.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+const assert = require("assert");
+const path = require("path");
+const fs = require("fs");
+
+const [, , targetPackage] = process.argv;
+
+const targetPackagePath = path.resolve(targetPackage, "package.json");
+
+(async () => {
+  assert(targetPackage, "No package path specified");
+  assert(
+    fs.existsSync(targetPackagePath),
+    `\`package.json\` not found. \npath: \`${targetPackagePath}\`\n`
+  );
+
+  const rootPkgJson = require("../package.json");
+  const targetPkgJson = require(targetPackagePath);
+
+  const newTargetPkgJson = mergePackageJson(targetPkgJson, rootPkgJson);
+
+  fs.writeFileSync(
+    targetPackagePath + "-",
+    JSON.stringify(newTargetPkgJson, null, 2)
+  );
+
+  function mergePackageJson(target, source) {
+    // publishConfig
+    const publishConfig = {
+      ...target.publishConfig,
+      registry: source.publishConfig.registry,
+    };
+
+    // config.commitizen
+    const config = {
+      ...target.config,
+      commitizen: source.config.commitizen,
+    };
+
+    // scripts.release
+    const scripts = {
+      ...target.scripts,
+      release: source.scripts.release,
+    };
+
+    return {
+      ...target,
+      publishConfig,
+      config,
+      scripts,
+    };
+  }
+})();
