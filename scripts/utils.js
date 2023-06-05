4a8825db2361af16e72c3f84f3ec911ae6dcf6ed
sudhyr <>
2023-06-05 22:43:50 +0800

    chore(release): fix workspace parse

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,12 +2,11 @@ const { promisify } = require("util");
 const exec = promisify(require("child_process").exec);
 
 async function workspacesInfo() {
-  const workspacesOutput = await exec("yarn workspaces info --json");
-
-  const lines = workspacesOutput.stdout.split(/\n/);
-  const workspacesStr = lines.slice(1, lines.length - 2).join("");
-
-  console.log(workspacesStr)
+  const workspacesOutput = (await exec("yarn workspaces info --json")).stdout;
+  const workspacesStr = workspacesOutput.substring(
+      workspacesOutput.indexOf('{'),
+      workspacesOutput.lastIndexOf('}')+1
+  )
 
   return JSON.parse(workspacesStr);
 }
