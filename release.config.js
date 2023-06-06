c80840f88aa929da5066df1e264d559f56c9d54e
sudhyr <>
2023-06-06 21:01:12 +0800

    chore(a1): test release config

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+
+        releaseRules: [
+          { type: "docs", scope: "README", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "fix", release: "patch" },
+        ],
+
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "packages/a1"
+      },
+    ],
+    // "@semantic-release/release-notes-generator"
+  ],
+};
